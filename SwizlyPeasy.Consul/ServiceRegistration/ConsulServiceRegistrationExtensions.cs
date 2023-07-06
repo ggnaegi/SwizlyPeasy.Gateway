578677b61c45efbc19c6dc57ccdafb9d33c76dd3
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2023-07-06 20:23:08 +0200

    Fixing some issues with consul service registration

--- SwizlyPeasy.Consul/ServiceRegistration/ConsulServiceRegistrationExtensions.cs
+++ SwizlyPeasy.Consul/ServiceRegistration/ConsulServiceRegistrationExtensions.cs
@@ -1,22 +1,29 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Consul;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using SwizlyPeasy.Common;
 using SwizlyPeasy.Common.Dtos;
 
-namespace SwizlyPeasy.Consul.ServiceRegistration
+namespace SwizlyPeasy.Consul.ServiceRegistration;
+
+/// <summary>
+///     Extension method for micro service registration
+///     in consul. You should make sure that the RegisterClient extension method
+///     is also called.
+/// </summary>
+public static class ConsulServiceRegistrationExtensions
 {
-    /// <summary>
-    /// Extension method for micro service registration
-    /// in consul. You should make sure that the RegisterClient extension method
-    /// is also called.
-    /// </summary>
-    public static class ConsulServiceRegistrationExtensions
+    public static void RegisterService(this IServiceCollection services, IConfiguration configuration)
     {
-        public static void RegisterService(this IServiceCollection services, IConfiguration configuration)
-        {
-            services.Configure<IOptions<ServiceRegistrationConfig>>(configuration.GetSection(Common.Constants.ServiceRegistrationConfigSection));
-            services.AddSingleton<IHostedService, ServiceDiscoveryHostedService>();
-        }
+        services.Configure<IOptions<ServiceRegistrationConfig>>(
+            configuration.GetSection(Constants.ServiceRegistrationConfigSection));
+
+        var serviceRegistrationConfig = new ServiceRegistrationConfig();
+        configuration.GetSection(Constants.ServiceRegistrationConfigSection).Bind(serviceRegistrationConfig);
+
+        services.AddSingleton<IHostedService, ServiceDiscoveryHostedService>(sp =>
+            new ServiceDiscoveryHostedService(sp.GetRequiredService<IConsulClient>(), serviceRegistrationConfig));
     }
 }
