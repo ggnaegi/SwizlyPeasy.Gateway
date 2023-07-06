587a4abc86bf6b4896f553b364f6c7dfe5ef01ae
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2023-07-06 20:23:08 +0200

    Fixing some issues with consul service registration

--- SwizlyPeasy.Consul/ServiceRegistration/ServiceDiscoveryHostedService.cs
+++ SwizlyPeasy.Consul/ServiceRegistration/ServiceDiscoveryHostedService.cs
@@ -1,63 +1,60 @@
 ﻿using Consul;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
 using SwizlyPeasy.Common.Dtos;
 
-namespace SwizlyPeasy.Consul.ServiceRegistration
+namespace SwizlyPeasy.Consul.ServiceRegistration;
+
+public class ServiceDiscoveryHostedService : IHostedService
 {
-    public class ServiceDiscoveryHostedService : IHostedService
+    private readonly IConsulClient _client;
+    private readonly ServiceRegistrationConfig _config;
+    private string _registrationId = "";
+
+    /// <summary>
+    /// </summary>
+    /// <param name="client"></param>
+    /// <param name="config"></param>
+    public ServiceDiscoveryHostedService(IConsulClient client, ServiceRegistrationConfig config)
     {
-        private readonly IConsulClient _client;
-        private readonly IOptions<ServiceRegistrationConfig> _config;
-        private string _registrationId;
+        _client = client ?? throw new ArgumentNullException(nameof(client));
+        _config = config ?? throw new ArgumentNullException(nameof(config));
+    }
 
-        /// <summary>
-        /// </summary>
-        /// <param name="client"></param>
-        /// <param name="config"></param>
-        /// <param name="registrationId"></param>
-        public ServiceDiscoveryHostedService(IConsulClient client, IOptions<ServiceRegistrationConfig> config, string registrationId)
+    /// <summary>
+    ///     registering service to consul.
+    ///     The registration id is defined by "service name" and "service id".
+    ///     This allows load balancing, with services grouped by service name.
+    ///     The service id must therefore be unique for a given service name.
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        _registrationId = $"{_config.ServiceName}-{_config.ServiceId}";
+
+        var registration = new AgentServiceRegistration
         {
-            _client = client;
-            _config = config;
-            _registrationId = registrationId;
-        }
-
-        /// <summary>
-        /// registering service to consul.
-        /// The registration id is defined by "service name" and "service id".
-        /// This allows load balancing, with services grouped by service name.
-        /// The service id must therefore be unique for a given service name.
-        /// </summary>
-        /// <param name="cancellationToken"></param>
-        public async Task StartAsync(CancellationToken cancellationToken)
-        {
-            _registrationId = $"{_config.Value.ServiceName}-{_config.Value.ServiceId}";
-
-            var registration = new AgentServiceRegistration
+            ID = _registrationId,
+            Name = _config.ServiceName,
+            Address = _config.ServiceAddress.Host,
+            Port = _config.ServiceAddress.Port,
+            Check = new AgentCheckRegistration
             {
-                ID = _registrationId,
-                Name = _config.Value.ServiceName,
-                Address = _config.Value.ServiceAddress.Host,
-                Port = _config.Value.ServiceAddress.Port,
-                Check = new AgentCheckRegistration
-                {
-                    HTTP = $"{_config.Value.ServiceAddress.Scheme}://{_config.Value.ServiceAddress.Host}:{_config.Value.ServiceAddress.Port}/{_config.Value.HealthCheckPath}",
-                    Interval = TimeSpan.FromSeconds(10)
-                }
-            };
+                HTTP =
+                    $"{_config.ServiceAddress.Scheme}://{_config.ServiceAddress.Host}:{_config.ServiceAddress.Port}/{_config.HealthCheckPath}",
+                Interval = TimeSpan.FromSeconds(10)
+            }
+        };
 
-            await _client.Agent.ServiceDeregister(registration.ID, cancellationToken);
-            await _client.Agent.ServiceRegister(registration, cancellationToken);
-        }
+        await _client.Agent.ServiceDeregister(registration.ID, cancellationToken);
+        await _client.Agent.ServiceRegister(registration, cancellationToken);
+    }
 
-        /// <summary>
-        /// unregistering the service from consul
-        /// </summary>
-        /// <param name="cancellationToken"></param>
-        public async Task StopAsync(CancellationToken cancellationToken)
-        {
-            await _client.Agent.ServiceDeregister(_registrationId, cancellationToken);
-        }
+    /// <summary>
+    ///     unregistering the service from consul
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await _client.Agent.ServiceDeregister(_registrationId, cancellationToken);
     }
 }
