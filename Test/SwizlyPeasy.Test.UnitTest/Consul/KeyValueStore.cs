04984d3e9d1b66988602a146fea94c12091d23e2
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2023-07-10 19:39:12 +0200

    Adding some unit tests for consul services and kv store.

---
+++ Test/SwizlyPeasy.Test.UnitTest/Consul/KeyValueStore.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using SwizlyPeasy.Common.Exceptions;
+
+namespace SwizlyPeasy.Test.UnitTest.Consul;
+
+public class KeyValueStore
+{
+    [Fact]
+    public async Task SaveToKeyValueStore_DoesNotThrowException()
+    {
+        var keyValueService = TestFactories.GetKeyValueService();
+        await keyValueService.SaveToKeyValueStore(TestFactories.RouteConfigKey,
+            Encoding.UTF8.GetBytes(TestFactories.RouteConfigString));
+    }
+
+    [Fact]
+    public async Task GetToKeyValueStore_UsingExistingKey_ReturnsResult()
+    {
+        var keyValueService = TestFactories.GetKeyValueService();
+        var valueBytes = await keyValueService.GetFromKeyValueStore(TestFactories.RouteConfigKey);
+
+        var stringValue = Encoding.UTF8.GetString(valueBytes);
+        Assert.Equal(TestFactories.RouteConfigString, stringValue);
+    }
+
+    [Fact]
+    public async Task GetToKeyValueStore_UsingNotExistingKey_ThrowsException()
+    {
+        var keyValueService = TestFactories.GetKeyValueService();
+        await Assert.ThrowsAsync<InternalDomainException>(async() => await keyValueService.GetFromKeyValueStore("test"));
+    }
+}
