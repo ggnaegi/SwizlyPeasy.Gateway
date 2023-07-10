b178c8e8709ba881483faeafc9dce8025e648567
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2023-07-10 19:39:12 +0200

    Adding some unit tests for consul services and kv store.

---
+++ Test/SwizlyPeasy.Test.UnitTest/Consul/Agents.cs
@@ -0,0 +1,69 @@
+﻿using Consul;
+
+namespace SwizlyPeasy.Test.UnitTest.Consul;
+
+public class Agents
+{
+    [Fact]
+    public async Task GetServices_NoRegistered_ReturnsEmptyDictionary()
+    {
+        var retrieveAgentsService = TestFactories.GetRetrieveAgentsService(new Dictionary<string, AgentService>());
+        var result = await retrieveAgentsService.RetrieveAgents();
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public async Task GetServices_SomeRegistered_ReturnsRegisteredInDictionary()
+    {
+        var agentsServicesDic = new Dictionary<string, AgentService>
+        {
+            { "TestService_1", TestFactories.GetAgentService("TestService_1", "TestService") },
+            { "SuperService_1", TestFactories.GetAgentService("SuperService_1", "SuperService") },
+            { "MegaService_1", TestFactories.GetAgentService("MegaService_1", "MegaService") }
+        };
+        var retrieveAgentsService = TestFactories.GetRetrieveAgentsService(agentsServicesDic);
+
+        var foundServices = await retrieveAgentsService.RetrieveAgents();
+
+        Assert.Equal(3, foundServices.Keys.Count);
+
+        Assert.Contains("TestService", foundServices.Keys);
+        Assert.Contains("SuperService", foundServices.Keys);
+        Assert.Contains("MegaService", foundServices.Keys);
+
+        Assert.Single(foundServices["TestService"]);
+        Assert.Single(foundServices["SuperService"]);
+        Assert.Single(foundServices["MegaService"]);
+
+        Assert.Equal("TestService_1", foundServices["TestService"].First().ID);
+        Assert.Equal("SuperService_1", foundServices["SuperService"].First().ID);
+        Assert.Equal("MegaService_1", foundServices["MegaService"].First().ID);
+    }
+
+    [Fact]
+    public async Task GetServices_WithSameServiceNamesButDifferentIds_ReturnsRegisteredGroupedByServiceNameDictionary()
+    {
+        var agentsServicesDic = new Dictionary<string, AgentService>
+        {
+            { "TestService_1", TestFactories.GetAgentService("TestService_1", "TestService") },
+            { "SuperService_1", TestFactories.GetAgentService("SuperService_1", "TestService") },
+            { "MegaService_1", TestFactories.GetAgentService("MegaService_1", "MegaService") }
+        };
+        var retrieveAgentsService = TestFactories.GetRetrieveAgentsService(agentsServicesDic);
+
+        var foundServices = await retrieveAgentsService.RetrieveAgents();
+
+        Assert.Equal(2, foundServices.Keys.Count);
+
+        Assert.Contains("TestService", foundServices.Keys);
+        Assert.DoesNotContain("SuperService", foundServices.Keys);
+        Assert.Contains("MegaService", foundServices.Keys);
+
+        Assert.Equal(2, foundServices["TestService"].Count);
+        Assert.Single(foundServices["MegaService"]);
+
+        Assert.Equal("TestService_1", foundServices["TestService"].First(x => x.ID == "TestService_1").ID);
+        Assert.Equal("SuperService_1", foundServices["TestService"].First(x => x.ID == "SuperService_1").ID);
+        Assert.Equal("MegaService_1", foundServices["MegaService"].First().ID);
+    }
+}
---
+++ Test/SwizlyPeasy.Test.UnitTest/TestFactories.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using Consul;
+using SwizlyPeasy.Consul.Agents;
+using SwizlyPeasy.Consul.KeyValueStore;
+using SwizlyPeasy.Test.UnitTest.Consul;
+
+namespace SwizlyPeasy.Test.UnitTest
+{
+    public static class TestFactories
+    {
+        public const string RouteConfigKey = "SwizlyPeasy.Key";
+        public const string RouteConfigString = "{\"Routes\":{\"route1\":{\"ClusterId\":\"DemoAPI\",\"AuthorizationPolicy\":\"oidc\",\"Match\":{\"Path\":\"/api/v1/demo/weather\"},\"Transforms\":[{\"RequestHeader\":\"Accept-Language\",\"Set\":\"de-CH\"}]},\"route2\":{\"ClusterId\":\"DemoAPI\",\"AuthorizationPolicy\":\"oidc\",\"Match\":{\"Path\":\"/api/v1/demo/weather-with-authorization\"},\"Transforms\":[{\"RequestHeader\":\"Accept-Language\",\"Set\":\"de-CH\"}]}}}";
+        public static KeyValueService GetKeyValueService()
+        {
+            var consulClient = ConsulClientFactory.GetKvConsulClient();
+            return new KeyValueService(consulClient);
+        }
+        public static RetrieveAgentsService GetRetrieveAgentsService(Dictionary<string, AgentService> servicesDic)
+        {
+            var consulClient = ConsulClientFactory.GetAgentsConsulClient(servicesDic);
+            return new RetrieveAgentsService(consulClient);
+        }
+        public static KVPair GetKvPair()
+        {
+            var routeConfigBytes = Encoding.UTF8.GetBytes(RouteConfigString);
+            return new KVPair(RouteConfigKey)
+            {
+                Value = routeConfigBytes
+            };
+        }
+
+        public static AgentService GetAgentService(string instanceId, string serviceName)
+        {
+            return new AgentService
+            {
+                Service = serviceName,
+                ID = instanceId
+            };
+        }
+    }
+}
