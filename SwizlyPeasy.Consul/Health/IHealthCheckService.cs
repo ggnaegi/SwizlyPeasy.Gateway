968145e5005de7d5be2d7275d5ee2e94f9c33c6e
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2023-07-11 20:09:32 +0200

    adding comments

--- SwizlyPeasy.Consul/Health/IHealthCheckService.cs
+++ SwizlyPeasy.Consul/Health/IHealthCheckService.cs
@@ -2,5 +2,11 @@
 
 public interface IHealthCheckService
 {
+    /// <summary>
+    /// Checking if service's destination is healthy
+    /// (checks are passing)
+    /// </summary>
+    /// <param name="serviceId"></param>
+    /// <returns></returns>
     public Task<bool> IsServiceHealthy(string serviceId);
 }
