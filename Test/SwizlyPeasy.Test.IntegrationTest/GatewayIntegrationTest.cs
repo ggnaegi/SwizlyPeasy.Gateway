537ac78d604b1137bd5ab513b55e9e649195be24
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2023-07-13 01:17:44 +0200

    adding integration tests

---
+++ Test/SwizlyPeasy.Test.IntegrationTest/GatewayIntegrationTest.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+using SwizlyPeasy.Common.Dtos;
+using SwizlyPeasy.Common.Dtos.Status;
+
+namespace SwizlyPeasy.Test.IntegrationTest;
+
+[Collection("TestHttpClient")]
+public class GatewayIntegrationTest : IClassFixture<TestHttpClient>
+{
+    private readonly TestHttpClient _httpClient;
+
+    public GatewayIntegrationTest(TestHttpClient httpClient)
+    {
+        _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+    }
+
+    [Fact]
+    public async Task Gateway_GetRoot_OkAndReturnsClusters()
+    {
+        var response = await _httpClient.Client.GetAsync("");
+
+        response.EnsureSuccessStatusCode();
+
+        var result = await response.Content.ReadAsStringAsync();
+
+        var serverStatus = JsonConvert.DeserializeObject<StatusDto>(result);
+
+        Assert.NotNull(serverStatus);
+
+        var storedStatus = JsonConvert.DeserializeObject<StatusDto>(Constants.GatewayRootResponse);
+
+        Assert.NotNull(storedStatus);
+
+        var timeNow = DateTime.Now;
+        serverStatus.StatusCheckedAt = timeNow;
+        storedStatus.StatusCheckedAt = timeNow;
+
+        Assert.Equal(storedStatus.ServiceDiscoveryAddress, serverStatus.ServiceDiscoveryAddress);
+
+        Assert.Equal(JsonConvert.SerializeObject(storedStatus), JsonConvert.SerializeObject(serverStatus));
+    }
+
+    [Fact]
+    public async Task Gateway_GetUnknownPath_NotFound()
+    {
+        var response = await _httpClient.Client.GetAsync("/test");
+
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+
+        var result = await response.Content.ReadAsStringAsync();
+
+        var rfcNotFound = JsonConvert.DeserializeObject<Rfc7807>(result);
+
+        Assert.NotNull(rfcNotFound);
+
+        Assert.Equal(404, rfcNotFound.Status);
+    }
+}
