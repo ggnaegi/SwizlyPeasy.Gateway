7a0ebd0fdd466bffba68b8fa71df92d56f216027
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2023-07-13 13:45:09 +0200

    code cleanup

--- Test/SwizlyPeasy.Test.IntegrationTest/TestHttpClient.cs
+++ Test/SwizlyPeasy.Test.IntegrationTest/TestHttpClient.cs
@@ -50,5 +50,5 @@ public class TestHttpClientCollection : ICollectionFixture<TestHttpClient>
 {
     // This class has no code, and is never created. Its purpose is simply
     // to be the place to apply [CollectionDefinition] and all the
-    // ICollectionFixture<> interfaces.}
+    // ICollectionFixture<> interfaces.
 }
