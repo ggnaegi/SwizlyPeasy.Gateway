18dd93c9606f96a1e4dd98a62e12fb0559922f86
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2023-07-14 15:58:46 +0200

    Moving dummy auth to Common package

--- Demo/SwizlyPeasy.Demo.API/Extensions/AuthorizationExtensions.cs
+++ Demo/SwizlyPeasy.Demo.API/Extensions/AuthorizationExtensions.cs
@@ -1,7 +1,4 @@
-﻿using System.Net;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using SwizlyPeasy.Demo.API.Authorization;
 
 namespace SwizlyPeasy.Demo.API.Extensions;
@@ -9,29 +6,16 @@ namespace SwizlyPeasy.Demo.API.Extensions;
 public static class AuthorizationExtensions
 {
     /// <summary>
-    ///     Adding demo authorization policy, checking if user's uid/sub matches bob's one
+    ///     Test policy, checking if user is bob
     /// </summary>
     /// <param name="services"></param>
-    public static void SetAuthenticationAndAuthorization(this IServiceCollection services)
+    public static void SetAuthorization(this IServiceCollection services)
     {
-        services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
-            {
-                options.Events.OnRedirectToAccessDenied = UnAuthorizedResponse;
-                options.Events.OnRedirectToLogin = UnAuthorizedResponse;
-            });
-
         services.AddAuthorization(options =>
         {
             options.AddPolicy("AreYouBob", policy =>
                 policy.Requirements.Add(new BobRequirement()));
         });
         services.AddSingleton<IAuthorizationHandler, AppAuthorizationHandler>();
-    }
-
-    private static Task UnAuthorizedResponse(RedirectContext<CookieAuthenticationOptions> context)
-    {
-        context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-        return Task.CompletedTask;
     }
 }
---
+++ SwizlyPeasy.Common/Auth/ClientAuthExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SwizlyPeasy.Common.Auth;
+
+public static class ClientAuthExtensions
+{
+    /// <summary>
+    ///     Setting "dummy" authentication
+    /// </summary>
+    /// <param name="services"></param>
+    public static void SetSwizlyPeasyAuthentication(this IServiceCollection services)
+    {
+        services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+            {
+                options.Events.OnRedirectToAccessDenied = UnAuthorizedResponse;
+                options.Events.OnRedirectToLogin = UnAuthorizedResponse;
+            });
+    }
+
+    private static Task UnAuthorizedResponse(RedirectContext<CookieAuthenticationOptions> context)
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+        return Task.CompletedTask;
+    }
+}
