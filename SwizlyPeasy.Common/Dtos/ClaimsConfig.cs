0f0d3076bc03c8c4f4d5e30dc49081b84a58fcfc
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2023-07-14 14:00:48 +0200

    New feature, allowing configuring oidc claims, choosing the ones sent as headers or mapped to claims

---
+++ SwizlyPeasy.Common/Dtos/ClaimsConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+using IdentityModel;
+
+namespace SwizlyPeasy.Common.Dtos;
+
+public class ClaimsConfig
+{
+    public string ClaimsHeaderPrefix { get; set; } = "SWIZLY-PEASY";
+    public string[] ClaimsAsHeaders { get; set; } =
+    {
+        JwtClaimTypes.Subject,
+        JwtClaimTypes.Email,
+        JwtClaimTypes.Name,
+        JwtClaimTypes.FamilyName
+    };
+
+    public Dictionary<string, string> JwtToIdentityClaimsMappings { get; set; } = new()
+    {
+        { JwtClaimTypes.Subject, ClaimTypes.NameIdentifier },
+        { JwtClaimTypes.Email, ClaimTypes.Email }
+    };
+}
