c9abed4b00bb60d5ff17428672b85502351e95b6
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2023-07-16 13:03:32 +0200

    adding Gateway Response No Services as dto. Tests passing

--- Test/SwizlyPeasy.Test.IntegrationTest/Constants.cs
+++ Test/SwizlyPeasy.Test.IntegrationTest/Constants.cs
@@ -2,6 +2,6 @@
 
 public static class Constants
 {
-    public const string GatewayRootResponse =
-        "{\"ServiceDiscoveryAddress\":\"http://consul:8500\",\"StatusCheckedAt\":\"2023-07-12T22:21:33.8033362+00:00\",\"Clusters\":[{\"ClusterId\":\"DemoAPI\",\"Healthy\":true,\"Destinations\":[{\"Healthy\":true,\"Id\":\"DemoAPI-1\",\"Address\":\"http://demo:80\"}]}]}";
+    public const string GatewayRootResponseNoServices =
+        "{\"ServiceDiscoveryAddress\":\"http://localhost:8500\",\"StatusCheckedAt\":\"2023-07-12T22:21:33.8033362+00:00\",\"Clusters\":[]}";
 }
