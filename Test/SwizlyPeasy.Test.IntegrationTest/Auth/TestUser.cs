fa30991f17b9db96418fb08d68ace0e8a292d382
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2023-07-18 22:39:23 +0200

    Adding authorization tests to integration tests.

---
+++ Test/SwizlyPeasy.Test.IntegrationTest/Auth/TestUser.cs
@@ -0,0 +1,22 @@
+﻿using IdentityModel;
+
+namespace SwizlyPeasy.Test.IntegrationTest.Auth;
+
+internal class TestUser
+{
+    internal string Sub { get; set; } = "1";
+    internal string Email { get; set; } = "henri.du.test@test.com";
+    internal string Name { get; set; } = "Henri";
+    internal string FamilyName { get; set; } = "Du Test";
+
+    internal Dictionary<string, object> GetClaims()
+    {
+        return new Dictionary<string, object>
+        {
+            { JwtClaimTypes.Subject, Sub },
+            { JwtClaimTypes.Name, Name }, 
+            { JwtClaimTypes.Email, Email },
+            { JwtClaimTypes.FamilyName, FamilyName }
+        };
+    }
+}
