67ccc8752da85a96e8f6005987dab230d87c1a3a
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2023-07-18 22:38:29 +0200

    adding a custom authentication handler for request headers, instead of using "dummy" cookies. It was a bad idea.

---
+++ SwizlyPeasy.Common/Auth/SwizlyPeasyAuthenticationOptions.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace SwizlyPeasy.Common.Auth;
+
+public class SwizlyPeasyAuthenticationOptions : AuthenticationSchemeOptions
+{
+    public const string DefaultScheme = "SwizlyPeasyAuthScheme";
+}
