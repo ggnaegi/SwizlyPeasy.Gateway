5fe8631e7c7619958ce22b30647b0a3d91583d10
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2023-07-19 03:22:12 +0200

    updating gateway response string, to support FakeAPI output

--- Test/SwizlyPeasy.Test.IntegrationTest/Constants.cs
+++ Test/SwizlyPeasy.Test.IntegrationTest/Constants.cs
@@ -3,7 +3,7 @@
 public static class Constants
 {
     public const string GatewayRootResponse =
-        "{\"ServiceDiscoveryAddress\":\"http://localhost:8500\",\"StatusCheckedAt\":\"2023-07-17T22:18:29.4745635+02:00\",\"Clusters\":[{\"ClusterId\":\"DemoAPI\",\"Healthy\":true,\"Destinations\":[{\"Healthy\":true,\"Id\":\"DemoAPI-1\",\"Address\":\"http://127.0.0.1:8020\"},{\"Healthy\":true,\"Id\":\"DemoAPI-2\",\"Address\":\"http://127.0.0.1:8030\"}]}]}";
+        "{\"ServiceDiscoveryAddress\":\"http://localhost:8500\",\"StatusCheckedAt\":\"2023-07-19T03:19:22.6738087+02:00\",\"Clusters\":[{\"ClusterId\":\"DemoAPI\",\"Healthy\":true,\"Destinations\":[{\"Healthy\":true,\"Id\":\"DemoAPI-1\",\"Address\":\"http://127.0.0.1:8020\"},{\"Healthy\":true,\"Id\":\"DemoAPI-2\",\"Address\":\"http://127.0.0.1:8030\"}]},{\"ClusterId\":\"FakeAPI\",\"Healthy\":false,\"Destinations\":[{\"Healthy\":false,\"Id\":\"FakeAPI-1\",\"Address\":\"http://127.0.0.1:8040\"}]}]}";
 
     public const string GatewayConfigWithPathSet =
         "{\"Routes\":{\"route1\":{\"ClusterId\":\"DemoAPI\",\"AuthorizationPolicy\":\"oidc\",\"Match\":{\"Path\":\"/api/v1/demo/weather\"},\"Transforms\":[{\"RequestHeader\":\"Accept-Language\",\"Set\":\"de-CH\"}]},\"route2\":{\"ClusterId\":\"DemoAPI\",\"AuthorizationPolicy\":\"oidc\",\"Match\":{\"Path\":\"/api/v1/demo/weather-with-authorization\"},\"Transforms\":[{\"RequestHeader\":\"Accept-Language\",\"Set\":\"de-CH\"}]},\"route3\":{\"ClusterId\":\"DemoAPI\",\"Match\":{\"Path\":\"/newpath\"},\"Transforms\":[{\"PathSet\":\"/api/v1/demo/weather-anonymous\"}]}}}";
