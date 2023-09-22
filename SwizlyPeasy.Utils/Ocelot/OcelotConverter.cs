7d4f89a2c716868207dc7eb253cd53f737684c6f
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2023-09-22 14:32:24 +0200

    adding simple ocelot config file converter, only for test purposes

---
+++ SwizlyPeasy.Utils/Ocelot/OcelotConverter.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using Ocelot.Configuration.File;
+using Yarp.ReverseProxy.Configuration;
+
+namespace SwizlyPeasy.Utils.Ocelot;
+
+public static class OcelotConverter
+{
+    /// <summary>
+    /// Helper method to convert an ocelot configuration file to a swizly configuration file
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public static string? ConvertOcelotConfigurationFile(string input)
+    {
+        var config = JsonConvert.DeserializeObject<FileConfiguration>(input);
+
+        if (config?.Routes == null) return null;
+
+        var ocelotRoutes = config.Routes;
+
+        Dictionary<string, RouteConfig> routeConfigsDic = new();
+
+        var id = 1;
+        foreach (var route in ocelotRoutes.Where(x => x.ServiceName != null))
+        {
+            var routeId = $"route{id}";
+            RouteConfig newConfig;
+
+            if (route.AuthenticationOptions is { AuthenticationProviderKey: not null })
+                newConfig = new RouteConfig
+                {
+                    ClusterId = route.ServiceName,
+                    AuthorizationPolicy = "oidc",
+                    Match = new RouteMatch
+                    {
+                        Path = route.DownstreamPathTemplate,
+                        Methods = route.UpstreamHttpMethod.Select(x => x.ToUpperInvariant()).ToArray()
+                    }
+                };
+            else
+                newConfig = new RouteConfig
+                {
+                    ClusterId = route.ServiceName,
+                    RateLimiterPolicy = "swizly1",
+                    Match = new RouteMatch
+                    {
+                        Path = route.DownstreamPathTemplate,
+                        Methods = route.UpstreamHttpMethod.Select(x => x.ToUpperInvariant()).ToArray()
+                    }
+                };
+
+            routeConfigsDic.Add(routeId, newConfig);
+            id++;
+        }
+
+        var baseDic = new Dictionary<string, object> { { "Routes", routeConfigsDic } };
+
+        var routeConfigString = JsonConvert.SerializeObject(baseDic, Formatting.Indented,
+            new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+
+        return routeConfigString;
+    }
+}
---
+++ SwizlyPeasy.Utils/Program.cs
@@ -0,0 +1,22 @@
+﻿using SwizlyPeasy.Utils.Ocelot;
+
+if (args.Length < 2)
+{
+    Console.WriteLine("Please provide the path to the ocelot config file and the path to the swizly config file.");
+    return;
+}
+
+if (!File.Exists(args[0]))
+{
+    throw new FileNotFoundException("Ocelot configuration file couldn't be found.");
+}
+
+if (!File.Exists(args[1]))
+{
+    throw new FileNotFoundException("Swizly configuration file couldn't be found.");
+}
+
+var input = File.ReadAllText(args[0]);
+var swizlyConfigurationString = OcelotConverter.ConvertOcelotConfigurationFile(input);
+
+File.WriteAllText(args[1], swizlyConfigurationString);
