d0004cbf7725d8af22e51aa29f393c3c629d7882
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2024-01-27 23:09:18 +0100

    some more warning fixes...

--- SwizlyPeasy.Gateway/Services/InMemoryConfig.cs
+++ SwizlyPeasy.Gateway/Services/InMemoryConfig.cs
@@ -6,9 +6,12 @@ namespace SwizlyPeasy.Gateway.Services;
 /// <summary>
 ///     Custom proxy config
 /// </summary>
-public class InMemoryConfig : IProxyConfig
+public class InMemoryConfig : IProxyConfig, IDisposable
 {
     private readonly CancellationTokenSource _cts = new();
+
+    // To detect redundant calls
+    private bool _disposedValue;
 
     public InMemoryConfig(IReadOnlyList<RouteConfig> routes, IReadOnlyList<ClusterConfig> clusters)
     {
@@ -24,5 +27,28 @@ public class InMemoryConfig : IProxyConfig
     internal void SignalChange()
     {
         _cts.Cancel();
+    }
+
+    // Public implementation of Dispose pattern callable by consumers.
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    // Protected implementation of Dispose pattern.
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposedValue)
+        {
+            return;
+        }
+
+        if (disposing)
+        {
+            _cts.Dispose();
+        }
+
+        _disposedValue = true;
     }
 }
